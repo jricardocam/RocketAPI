8d673589e5f2f7a3774626e3a9feaea7a7a315ea
jricardocam <jricardocam@gmail.com>
2022-08-03 21:06:20 -0500

    Funcionando, haciendo pruebas con PostMan

--- src/database.js
+++ src/database.js
@@ -18,49 +18,4 @@ mysqlConnection.connect(function (err) {
   }
 });
 
-router.post('/', (req, res) => {
-  const{ID, NOMBRE,SEGUNDORNOMBRE,APELLIDOPATERNO,APELLIDOMATERNO,FECHANACIMIENTO,EMAIL,TELEFONO} = req.body;
-  const query= `
-    SET @ID = ?;
-    SET @NOMBRE = ?;
-    SET @SEGUNDORNOMBRE = ?;
-    SET @APELLIDOPATERNO = ?;
-    SET @APELLIDOMATERNO = ?;
-    SET @FECHANACIMIENTO = ?;
-    SET @EMAIL = ?;
-    SET @TELEFONO = ?;
-    CALL USUARIOADDOREDIT(@ID, @NOMBRE, @SEGUNDORNOMBRE, @APELLIDOPATERNO, @APELLIDOMATERNO, @FECHANACIMIENTO, @EMAIL, @TELEFONO);
-    `;
-  mysqlConnection.query(query,[ID, NOMBRE,SEGUNDORNOMBRE,APELLIDOPATERNO,APELLIDOMATERNO,FECHANACIMIENTO,EMAIL,TELEFONO],(err, rows, fields) => {
-    if (!err) {
-      res.json({Status: 'Usuario agregado'});
-    } else {
-      console.log(err);
-    }
-  });
-});
-
-router.put('/:ID', (req, res) => {
-  const{ID, NOMBRE,SEGUNDORNOMBRE,APELLIDOPATERNO,APELLIDOMATERNO,FECHANACIMIENTO,EMAIL,TELEFONO} = req.body;
-  const { ID } = req.params;
-  const query= 'CALL USUARIOUPDATE(?,?,?,?,?,?,?,?)';
-  mysqlConnection.query(query,[ID, NOMBRE,SEGUNDORNOMBRE,APELLIDOPATERNO,APELLIDOMATERNO,FECHANACIMIENTO,EMAIL,TELEFONO],(err, rows, fields) => {
-    if (!err) {
-      res.json({Status: 'Usuario actualizado'});
-    } else {
-      console.log(err);
-    }
-  });
-});
-
-router.delete('/:ID', (req, res) => {
-  const { ID } = req.params;
-  mysqlConnection.query('DELETE FROM usuarios WHERE ID = ?', [ID], (err, rows, fields) => {
-    if (!err) {
-      res.json({Status: 'Usuario eliminado'});
-    } else {
-      console.log(err);
-    }
-  });
-});
 module.exports = mysqlConnection;
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -15,7 +15,6 @@ router.get('/', (req, res) => {
 
 router.get('/:ID', (req, res) => {
     const { ID } =req.params;
-    console.log(ID);
     mysqlConnection.query('SELECT * FROM usuarios WHERE ID = ?', [ID], (err, rows, fields) => {
         if (!err) {
             res.json(rows[0]);
@@ -24,5 +23,52 @@ router.get('/:ID', (req, res) => {
         }
     });
 });
+
+router.post('/', (req, res) => {
+    const{ID, NOMBRE,SEGUNDORNOMBRE,APELLIDOPATERNO,APELLIDOMATERNO,FECHANACIMIENTO,EMAIL,TELEFONO} = req.body;
+    const query= `
+      SET @ID = ?;
+      SET @NOMBRE = ?;
+      SET @SEGUNDORNOMBRE = ?;
+      SET @APELLIDOPATERNO = ?;
+      SET @APELLIDOMATERNO = ?;
+      SET @FECHANACIMIENTO = ?;
+      SET @EMAIL = ?;
+      SET @TELEFONO = ?;
+      CALL USUARIOADDOREDIT(@ID, @NOMBRE, @SEGUNDORNOMBRE, @APELLIDOPATERNO, @APELLIDOMATERNO, @FECHANACIMIENTO, @EMAIL, @TELEFONO);
+      `;
+    mysqlConnection.query(query,[ID, NOMBRE,SEGUNDORNOMBRE,APELLIDOPATERNO,APELLIDOMATERNO,FECHANACIMIENTO,EMAIL,TELEFONO],(err, rows, fields) => {
+      if (!err) {
+        res.json({Status: 'Usuario agregado'});
+      } else {
+        console.log(err);
+      }
+    });
+  });
+  
+  router.put('/:ID', (req, res) => {
+    const{I, NOMBRE,SEGUNDORNOMBRE,APELLIDOPATERNO,APELLIDOMATERNO,FECHANACIMIENTO,EMAIL,TELEFONO} = req.body;
+    const { ID } = req.params;
+    const query= 'CALL USUARIOUPDATE(?,?,?,?,?,?,?,?)';
+    mysqlConnection.query(query,[ID, NOMBRE,SEGUNDORNOMBRE,APELLIDOPATERNO,APELLIDOMATERNO,FECHANACIMIENTO,EMAIL,TELEFONO],(err, rows, fields) => {
+      if (!err) {
+        res.json({Status: 'Usuario actualizado'});
+      } else {
+        console.log(err);
+      }
+    });
+    
+  });
+  
+  router.delete('/:ID', (req, res) => {
+    const { ID } = req.params;
+    mysqlConnection.query('DELETE FROM usuarios WHERE ID = ?', [ID], (err, rows, fields) => {
+      if (!err) {
+        res.json({Status: 'Usuario eliminado'});
+      } else {
+        console.log(err);
+      }
+    });
+  });
 
 module.exports = router;
